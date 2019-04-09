d72f1b77dc0843e7fdb10ac9a48724dbc103050b
Jack Gamboa <jgamboa929@gmail.com>
2019-04-09 15:42:53 -0500

    Dynamically update burger with ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,36 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
+  // Consume ingredients object.  Ingredient x Qty.
+  const transformedIngredients = () => {
+    let ingredientQtyListEach = [];
+    for (let ig in props.ingredients) {
+      for (let i = 0; i < props.ingredients[ig]; i++) {
+        ingredientQtyListEach.push(ig);
+      }
+    }
+
+    return ingredientQtyListEach.map((igKey, index) => {
+      return <BurgerIngredient key={igKey + index} type={igKey} />;
+    });
+  };
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients()}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
 };
 
 export default burger;
+
+/*
+  // Reference alternative code
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+*/
