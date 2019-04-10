1f21350befac102917887f27444978f59009f2b5
Jack Gamboa <jgamboa929@gmail.com>
2019-04-10 10:44:14 -0500

    Build controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Mead", type: "meat" }
+];
+
+const buildControlsList = controls.map(control => (
+  <BuildControl key={control.label} label={control.label} />
+));
+
+const buildControls = props => (
+  <div className={classes.BuildControls}>{buildControlsList}</div>
+);
+
+export default buildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,24 +3,27 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
-  // Consume ingredients object.  Ingredient x Qty.
-  const transformedIngredients = () => {
-    let ingredientQtyListEach = [];
-    for (let ig in props.ingredients) {
-      for (let i = 0; i < props.ingredients[ig]; i++) {
-        ingredientQtyListEach.push(ig);
-      }
+  // Consume ingredients object. Create a final array of ingredient types (one array entry per ingredient quantity (ie even on same ingredient))
+  let ingredientQtyListEach = [];
+  for (let ig in props.ingredients) {
+    for (let i = 0; i < props.ingredients[ig]; i++) {
+      ingredientQtyListEach.push(ig);
     }
+  }
 
-    return ingredientQtyListEach.map((igKey, index) => {
-      return <BurgerIngredient key={igKey + index} type={igKey} />;
-    });
-  };
+  let transformedIngredients = ingredientQtyListEach.map((igKey, index) => {
+    return <BurgerIngredient key={igKey + index} type={igKey} />;
+  });
+
+  // Default to handle empty array of ingredients
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      {transformedIngredients()}
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
@@ -30,9 +33,20 @@ export default burger;
 
 /*
   // Reference alternative code
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+    let transformedIngredients = Object.keys(props.ingredients)
+      .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+          return <BurgerIngredient key={igKey + i} type={igKey} />;
+        });
+      })
+      .reduce((arr, el) => {
+        return arr.concat(el);
+      }, []);
+
+    console.log(transformedIngredients);
+
+    // Default to handle empty array of ingredients
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
 */
