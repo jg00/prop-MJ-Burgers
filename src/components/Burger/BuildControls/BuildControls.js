8a59d385c1eb94943868206f67c103c3273d9aec
Jack Gamboa <jgamboa929@gmail.com>
2019-04-13 11:53:58 -0500

    BuildControl add/remove ingredient.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -9,12 +9,20 @@ const controls = [
   { label: "Meat", type: "meat" }
 ];
 
-const buildControlsList = controls.map(control => (
-  <BuildControl key={control.label} label={control.label} />
-));
+const buildControlsList = props => {
+  return controls.map(control => (
+    <BuildControl
+      key={control.label}
+      label={control.label}
+      added={() => props.ingredientAdded(control.type)}
+      removed={() => props.ingredientRemoved(control.type)}
+      disabled={props.disabled[control.type]}
+    />
+  ));
+};
 
 const buildControls = props => (
-  <div className={classes.BuildControls}>{buildControlsList}</div>
+  <div className={classes.BuildControls}>{buildControlsList(props)}</div>
 );
 
 export default buildControls;
