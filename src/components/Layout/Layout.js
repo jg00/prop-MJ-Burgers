15b108064807fe36b60551b04b2fcbaf81e67e8c
Jack Gamboa <jgamboa929@gmail.com>
2019-04-25 16:07:12 -0500

    Hide side drawer, add backdrop to side drawer.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
+
+/*
 const layout = props => (
   <Aux>
     <Toolbar />
@@ -13,3 +41,4 @@ const layout = props => (
 );
 
 export default layout;
+*/
--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,6 +1,12 @@
 import React from "react";
 import classes from "./Backdrop.module.css";
 
+/*
+  References:
+    BurgerBuilder > Modal > BackDrop
+    Layout > SideDrawer > BackDrop
+*/
+
 const backdrop = props =>
   props.show ? (
     <div className={classes.Backdrop} onClick={props.clicked} />
