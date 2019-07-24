ebc04d65f40eb63787407e67a3076e2ead1f3294
Jack Gamboa <jgamboa929@gmail.com>
2019-07-24 15:05:44 -0500

    reducers - refactor code using utility function updateObject.

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   ingredients: null,
@@ -17,11 +18,26 @@ const INGREDIENT_PRICES = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      /*
-              [action.ingredientName] > This is ES6 syntax that lets you dynamically overwrite a property in a given JS object {}
-              You provide a reference to the property inside of [] that you want to update
-              [] here this does 'not' create an array.  This is just the syntax that allows you to reference a property name that you want to update.
-          */
+      const updatedIngredient = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+      }; // Has to be an object in order for updateObject() to function correctly
+      const updatedIngredients = updateObject(
+        state.ingredients,
+        updatedIngredient
+      );
+      const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+      };
+
+      return updateObject(state, updatedState);
+
+    /*
+        [action.ingredientName] > This is ES6 syntax that lets you dynamically overwrite a property in a given JS object {}
+        You provide a reference to the property inside of [] that you want to update
+        [] here this does 'not' create an array.  This is just the syntax that allows you to reference a property name that you want to update.
+      */
+    /* Original version before using updateObject() in utility.js
       return {
         ...state,
         ingredients: {
@@ -30,8 +46,10 @@ const reducer = (state = initialState, action) => {
         },
         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
       };
+      */
 
     case actionTypes.REMOVE_INGREDIENT:
+      // Kept original version and not using updateObject()
       return {
         ...state,
         ingredients: {
@@ -42,9 +60,7 @@ const reducer = (state = initialState, action) => {
       };
 
     case actionTypes.SET_INGREDIENTS:
-      return {
-        ...state,
-        // ingredients: action.ingredients, // displays in the order returned.
+      return updateObject(state, {
         ingredients: {
           salad: action.ingredients.salad,
           bacon: action.ingredients.bacon,
@@ -53,13 +69,32 @@ const reducer = (state = initialState, action) => {
         },
         totalPrice: 4,
         error: false
-      };
+      });
+
+    /* Original version
+      return {
+          ...state,
+          // ingredients: action.ingredients, // displays in the order returned.
+          ingredients: {
+            salad: action.ingredients.salad,
+            bacon: action.ingredients.bacon,
+            cheese: action.ingredients.cheese,
+            meat: action.ingredients.meat
+          },
+          totalPrice: 4,
+          error: false
+        };
+      */
 
     case actionTypes.FETCH_INGREDIENTS_FAILED:
+      return updateObject(state, { error: true });
+
+    /* Original version
       return {
         ...state,
         error: true
       };
+    */
 
     default:
       return state;
@@ -67,3 +102,27 @@ const reducer = (state = initialState, action) => {
 };
 
 export default reducer;
+
+/* Alternative to condence the switch statement in the reducer
+  const addIngredient = (state, action) => {
+    const updatedIngredient = {
+      [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+    }; // Has to be an object in order for updateObject() to function correctly
+    const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+    const updatedState = {
+      ingredients: updatedIngredients,
+      totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    };
+
+    return updateObject(state, updatedState);
+  };
+
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case actionTypes.ADD_INGREDIENT:
+        return addIngredient(state, action);
+      case actionTypes.REMOVE_INGREDIENT:
+        return removeIngredient(state, action);
+    }
+  };
+*/
