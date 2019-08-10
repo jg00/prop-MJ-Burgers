9061ecfc60c134bd211dd0f816e1a25342bb4c6b
Jack Gamboa <jgamboa929@gmail.com>
2019-08-10 15:47:32 -0500

    auth.js action - use localStore for user authentication and app refresh.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,6 +25,10 @@ export const authFail = error => {
 };
 
 export const logout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("expirationDate");
+  localStorage.removeItem("userId");
+
   return {
     type: actionTypes.AUTH_LOGOUT
   };
@@ -63,8 +67,15 @@ export const auth = (email, password, isSignup) => {
       .then(response => {
         console.log(response.data);
 
-        // Store token, expiration, userid
+        // Return a Date object represending the calculated expiration date.  (expiresIn (seconds)) * 1000  converted to milliseconds b/c JavaScript time works in milliseconds.
+        const expirationDate = new Date(
+          new Date().getTime() + response.data.expiresIn * 1000
+        );
+
+        // Store token, expirationDate(based on ExpiresIn Info), userid
         localStorage.setItem("token", response.data.idToken);
+        localStorage.setItem("expirationDate", expirationDate);
+        localStorage.setItem("userId", response.data.localId);
 
         dispatch(authSuccess(response.data.idToken, response.data.localId)); // Fields - idToken, localId
         dispatch(checkAuthTimeout(response.data.expiresIn)); // expiresIn: '3600' (3600 secs from Firebase)
@@ -80,5 +91,37 @@ export const setAuthRedirectPath = path => {
   return {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path
+  };
+};
+
+// Called from Auth.js on load
+// Whenever we load the app and/or user logs in we want to check
+// a. If token has not expired then update the state of the user in the redux store (dispatch AUTH_SUCCESS).
+// b. also, make sure 'automatic' log out of user is enabled (ie the timer)
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem("expirationDate")); // returns string.  Convert to Date object.
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      }
+      // Set redux store
+      else {
+        const userId = localStorage.getItem("userId");
+
+        // For 'auto' log in of user upon app refresh
+        dispatch(authSuccess(token, userId));
+
+        // For starting timer for 'auto' logging out of user upon token expiration
+        dispatch(
+          checkAuthTimeout(
+            (expirationDate.getTime() - new Date().getTime()) / 1000
+          )
+        ); // Returns difference in milliseconds but we need to pass seconds to checkAuthTimeout()
+      }
+    }
   };
 };
